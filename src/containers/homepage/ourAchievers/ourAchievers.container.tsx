697fddc2d5c125837004fdc844a2fc34eed4923c
alikhan866 <ok.66677788@gmail.com>
2024-01-17 02:08:26 +0530

    fixed stylings

--- src/containers/homepage/ourAchievers/ourAchievers.container.tsx
+++ src/containers/homepage/ourAchievers/ourAchievers.container.tsx
@@ -4,14 +4,13 @@ import classes from './ourAchievers.module.scss'
 import { motion } from 'framer-motion';
 import { ourAchieversData } from ".";
 
-
 export const OurAchieversContainer: React.FC = () => {
     return (
         <div className={classes.mainContainer}>
-            {ourAchieversData.map((item, index) => {
+            {ourAchieversData.map((item) => {
                 return (
                     <motion.div
-                        key={index}
+                        key={Math.random()}
                         initial={{ opacity: 0, y: item.y }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ type: "spring", stiffness: item.stifness }}
@@ -25,31 +24,3 @@ export const OurAchieversContainer: React.FC = () => {
 
     )
 }
-{/* <motion.div
-    initial={{ opacity: 0, y: 150 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ type: "spring", stiffness: 80 }}
->
-    <StudentCard college="IIT DELHI" name="Ayush Kumar Paratha" rank={5} image={IMG} color="hsl(118, 98%, 25%)" />
-</motion.div>
-<motion.div
-    initial={{ opacity: 0, y: 250 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ type: "spring", stiffness: 70 }}
->
-    <StudentCard college="IIT DELHI" name="Ayush Kumar Paratha" rank={5} image={IMG} />
-</motion.div>
-<motion.div
-    initial={{ opacity: 0, y: 450 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ type: "spring", stiffness: 60 }}
->
-    <StudentCard college="IIT DELHI" name="Ayush Kumar Paratha" rank={1346} image={IMG} />
-</motion.div>
-<motion.div
-    initial={{ opacity: 0, y: 650 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ type: "spring", stiffness: 50 }}
->
-    <StudentCard college="IIT MUMBAI" name="Ayush Kumar Paratha" rank={565} image={IMG} />
-</motion.div> */}
---
+++ src/hooks/buttonResize.ts
@@ -0,0 +1,21 @@
+import { useWindowSize } from 'usehooks-ts';
+
+export const useButtonResize = () => {
+  const { width } = useWindowSize();
+  if (width < 360) {
+    return 'xxs';
+  }
+  if (width < 420) {
+    return 'xs';
+  }
+  if (width < 768) {
+    return 'sm';
+  }
+  if (width < 1024) {
+    return 'sm';
+  }
+  if (width > 1024) {
+    return 'md';
+  }
+  return 'md';
+};
