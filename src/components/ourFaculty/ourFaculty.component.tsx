1785e2490c59b106dd668a70f922b48fa46c0c4a
Mayank Kumar <ok.66677788@gmail.com>
2024-01-24 01:32:14 +0530

    made responsive the ourFaculty heading

--- src/components/ourFaculty/ourFaculty.component.tsx
+++ src/components/ourFaculty/ourFaculty.component.tsx
@@ -10,7 +10,7 @@ export const OurFaculty: React.FC<IourFacultyData> = (props: IourFacultyData) =>
 
     <div className={classes.imgcontainer}>
        <img className={classes.teachers_image} src={image2} alt="teachers_image" />
-       <img className={classes.bg_img} src={image}alt="background image" />
+       <img className={classes.bg_img} src={image}alt="back ground image" />
     </div >
 
 
--- src/layout/footer/footer.layout.tsx
+++ src/layout/footer/footer.layout.tsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Footer: React.FC = () => {
     return (
         <div style={{ width: '100%', padding: '5px 2px 5px' }}>
-            Footer
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore maxime aliquid eveniet sequi eaque laborum animi veniam? Nihil, id atque natus veniam dicta ut, nesciunt consectetur omnis quasi, error ipsa.
         </div>
 
     )
