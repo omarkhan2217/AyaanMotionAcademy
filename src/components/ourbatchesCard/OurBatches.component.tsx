1419f98053e51f606d647f4739d5398ff00454a4
Omar Khan <67383583+omarkhan2217@users.noreply.github.com>
2025-03-12 13:28:34 +0530

    Update OurBatches.component.tsx

--- src/components/ourbatchesCard/OurBatches.component.tsx
+++ src/components/ourbatchesCard/OurBatches.component.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./ourBatches.module.scss";
+import classes from "./OurBatches.module.scss";
 import { ISBatchesCardProps } from "../../types";
 import { Button } from "..";
 import { ArrowIcon, CheckMark } from "../../assets";
