66b18bfb4d07294bdc49849bdda53225026d59b4
Omar Khan <67383583+omarkhan2217@users.noreply.github.com>
2025-03-12 13:09:52 +0530

    Update rankersBatch.container.tsx

--- src/containers/ourBatches/rankersBatch/rankersBatch.container.tsx
+++ src/containers/ourBatches/rankersBatch/rankersBatch.container.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./rankersbatch.module.scss";
+import classes from "./rankersBatch.module.scss";
 import {
   BatchesFacultyComponent,
   BatchesFeatureCardComponent,
