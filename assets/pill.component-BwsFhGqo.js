713162b8844fdcc9a582df2fc2790e4240f50ec0
omarkhan2217 <ok.66677788@gmail.com>
2025-03-13 11:11:07 +0530

    Updates

--- assets/pill.component-BwsFhGqo.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-D_H1y8ot.js";const t="_mainContainer_efxw5_1",i={mainContainer:t},e=n=>{const{label:o,pillColor:s}=n;return a.jsx("div",{className:i.mainContainer,style:{backgroundColor:s},children:o})};export{e as C};
